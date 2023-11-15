5dd6b464aab9ff0503f7657881a8f66ef26b8ac4
dongjun6343 <dongjun6343@naver.com>
2023-11-15 21:03:56 +0900

    1. 회원 도메인 및 리포지토리 만들기 2. 회원 리포지토리 테스트 케이스 작성

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,4 +21,42 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; // "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        // getter/setter - 자바 빈.
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
+
+
+/**
+ * @ResponseBody
+ * 내장 톰캣 -> helloController -> HttpMessageConvert 동작. (JsonConverter, StringConverter)
+ *
+ * 객체가 오면 JsonConverter가 디폴트로 json형식으로 만들어서 웹 브라우저한테 응답을 준다.
+ *
+ * Spring은 Jackson을 기본으로 씀.
+ *
+ */
