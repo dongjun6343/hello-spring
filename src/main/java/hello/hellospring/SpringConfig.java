a12b54a5eca48f7885e84ad4c5c4080a6ae949b5
dongjun6343 <dongjun6343@naver.com>
2023-11-22 21:19:46 +0900

    자바 코드로 직접 스프링 빈 등록하기.

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,25 @@
+package hello.hellospring;
+
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    // 자바 코드로 직접 스프링 빈 등록하기.
+    // DI에는 필드 주입 , setter 주입, 생성자 주입이 있다. -> 의존관계가 실행중에 동적으로 변하는 경우는 없으므로 생성자 주입을 권장한다.
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
